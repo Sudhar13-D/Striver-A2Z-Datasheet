a120ba9af81f7c52609006df5bc76a5dccb89ad3
Sudhar13-D <sudharsand25012007@gmail.com>
2026-01-13 23:13:19 +0530

    Pattern finished

--- Patterns/Pattern.java
+++ Patterns/Pattern.java
@@ -29,7 +29,7 @@ class Pattern{
         p.patten19(n);
         p.patten20(n);
         p.patten21(n);
-        //p.patten22(n);
+        p.patten22(n);
         
         
         sc.close();
@@ -288,6 +288,19 @@ class Pattern{
             System.out.println();
         }
         }
-        
     }
+    public void patten22(int n){
+        for(int i =0;i<2*n-1;i++){
+            for(int j = 0;j<2*n-1;j++){
+            int top =i;
+            int left = j;
+            int right = (2*n -2) - j;
+            int down = (2*n-2) - i;
+            int min = Math.min(Math.min(top, down), Math.min(left, right));
+            System.out.print(min);
+            System.out.print((n - min)+" ");
+        }
+        System.out.println();
+    }
+}
 }

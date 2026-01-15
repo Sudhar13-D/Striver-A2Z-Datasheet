1a0470781584be284069270d9d3b6c2ba36e2a81
Sudhar13-D <sudharsand25012007@gmail.com>
2026-01-15 23:10:35 +0530

    7 basic Maths Problem

--- Basics/BasicMaths.java
+++ Basics/BasicMaths.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class BasicMaths {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n:");
         int n = sc.nextInt();
+        System.out.print("Enter a,b for GCD:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int dup = n;
         System.out.print(countDigits(n));
         System.out.println();
@@ -17,15 +21,21 @@ public class BasicMaths {
         System.out.println();
         int AmstrongNo = AmstrongNumber(n);
         if (dup == AmstrongNo) {
-            System.out.println("AmstrongNumber");
+            System.out.print("AmstrongNumber");
         }
         else System.out.print("Not an AmstrongNumber");
+         System.out.println();
+        printAllDivisor(n);
+        System.out.println();
+        printPrimeNumbers(n);
 
+        printGCD(a,b);
+        sc.close();    
     }
     static int countDigits(int n){
         int count =0;
         while(n > 0){
-            int digit = n % 10;
+            int digits = n % 10;
             count++;  
             n = n/10; //this is the place where main action comes it is division where log comes into play
              // Time complexity = O(log10(n))  --> log10 because n divides by 10 -> if n/5 then log5 
@@ -50,4 +60,33 @@ public class BasicMaths {
         }
         return AmstNum;
     } 
+    static void printAllDivisor(int n){
+        for(int i = 1;i <= n;i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    static void printPrimeNumbers(int n){
+        int count = 0;
+        System.out.print("Prime Numbers ");
+        for(int i = 1;i*i<n;i++){
+            if(n%i == 0){
+                count++;
+                if((n/i) != i) count++;
+            }
+    } 
+        if(count == 2) System.out.println("True");
+        else System.out.println("False");
+    }
+
+    static void printGCD(int a,int b){
+        while(a > 0 && b > 0){
+            if(a > b) a = a % b;
+            else b = b % a;
+        }
+            if(a == 0) System.out.print(b);
+            else System.out.print(a);
+        
+    }
 }

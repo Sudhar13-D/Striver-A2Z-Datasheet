b736d1430ba456fd4915c1448889fdc3e0acb103
Sudhar13-D <sudharsand25012007@gmail.com>
2026-01-17 22:22:40 +0530

    Backtracking

--- Basics/BasicRecursion.java
+++ Basics/BasicRecursion.java
@@ -9,8 +9,11 @@ public class BasicRecursion {
         int n = sc.nextInt();
         System.out.println("Numbers from 1 to " + n + ": ");
         print(n);
-
-
+        printName(1,n);
+        System.out.println("Numbers from "+n+"to 1" + ": ");
+        printNto1(1, n);
+        System.out.println("Numbers name 1 to " + n + ": ");
+        print1ToN(n, n);
         sc.close();
     }
     static int cnt = 1;
@@ -22,4 +25,27 @@ public class BasicRecursion {
         cnt++;
         print(n);
     }
+    static void printName(int i,int n){
+        if(i>n){
+        return;
+        }
+        System.out.println("Sudharsan");
+        printName(i+1,n);
+    }
+    //backtracking
+    static void print1ToN(int i,int n){
+        if(i < 1){
+        return;
+        }
+        print1ToN(i-1,n);
+        System.out.println(i);
+    }
+    static void printNto1(int i,int n){
+        if(i > n){
+        return;
+        }
+        printNto1(i+1,n);
+        System.out.println(i);
+    }
+    
 }

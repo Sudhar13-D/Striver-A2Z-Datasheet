07335a5aa693e884090a34d8e1c4943a60f68d7c
Sudhar13-D <sudharsand25012007@gmail.com>
2026-01-17 22:09:12 +0530

    basic recursion

--- Basics/BasicRecursion.java
+++ Basics/BasicRecursion.java
@@ -1,7 +1,25 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class BasicRecursion {
     public static void main(String[] args) {
         System.out.println("Basic Recursions");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Numbers from 1 to " + n + ": ");
+        print(n);
+
+
+        sc.close();
+    }
+    static int cnt = 1;
+    static void print(int n){
+        if(cnt == n+1){
+        return;
+        }
+        System.out.println(cnt);
+        cnt++;
+        print(n);
     }
 }
